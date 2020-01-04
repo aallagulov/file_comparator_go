1f88eadb6a02cb177e7b1dfdd0c9817e0692a0e0
Anvar Allagulov <aallagulov.relap@gmail.com>
2020-01-05 01:28:26 +0300

    1st dummy version

---
+++ main.go
@@ -0,0 +1,81 @@
+package main
+
+import (
+	"bytes"
+	"fmt"
+	"sort"
+	"strings"
+	"sync"
+)
+
+// SafeCounter is safe to use concurrently.
+type SafeCounter struct {
+	v   map[string]int
+	mux sync.Mutex
+}
+
+func (c *SafeCounter) Inc(key string) {
+	c.mux.Lock()
+	c.v[key]++
+	c.mux.Unlock()
+}
+
+// func (c *SafeCounter) Value(key string) int {
+// 	c.mux.Lock()
+// 	defer c.mux.Unlock()
+// 	return c.v[key]
+// }
+
+func (c *SafeCounter) Stat() string {
+	// we need to return the result sorted by values
+	wordFrequencies := c.v
+	countersMap := make(map[int]string, len(wordFrequencies))
+	counters := make([]int, 0, len(wordFrequencies))
+	for k, v := range wordFrequencies {
+		counters = append(counters, v)
+		countersMap[v] = k
+	}
+
+	sort.Sort(sort.Reverse(sort.IntSlice(counters)))
+
+	b := new(bytes.Buffer)
+	for i := 0; i < len(counters); i++ {
+		counter := counters[i]
+		fmt.Fprintf(b, "%s=\t%d\n", countersMap[counter], counter)
+	}
+	return b.String()
+}
+
+func main() {
+	// f1_path := flag.String("f1", "", "path to the 1st file")
+	// f2_path := flag.String("f2", "", "path to the 2nd file")
+
+	str1 := "asd\t qwe zxc\n asd"
+	str2 := "asd cat  tac asd"
+
+	sl1 := strings.Fields(str1)
+	sl2 := strings.Fields(str2)
+
+	fmt.Println(sl1)
+	fmt.Println(sl2)
+
+	var wg sync.WaitGroup
+	wg.Add(2)
+
+	c := SafeCounter{v: make(map[string]int)}
+
+	countFunc := func(words []string) {
+		defer wg.Done()
+		for i := 0; i < len(words); i++ {
+			fmt.Println(words[i])
+			c.Inc(words[i])
+		}
+	}
+
+	go countFunc(sl1)
+	go countFunc(sl2)
+
+	wg.Wait()
+
+	fmt.Println(c.Stat())
+}
